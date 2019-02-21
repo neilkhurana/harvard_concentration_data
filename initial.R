be7adae7cd71254e81697eb0615397236c3b66d2
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-21 00:00:33 -0500

    Cleaned up axis labels and included numbers within bars for each concentration.

--- initial.R
+++ initial.R
@@ -10,8 +10,12 @@ concentrations <- read_xlsx("concentrations_h.xlsx") %>%
 hard_sciences <- concentrations %>% 
   filter(concentration %in% c("Molecular and Cellular Biology", "Chemistry", "Physics")) 
 hard_sciences_plot<- 
-  ggplot(hard_sciences, aes(x = year, y = count, fill = concentration)) + 
-  geom_bar(stat = "identity"); plot(hard_sciences_plot)
+  ggplot(hard_sciences, aes(x = year, y = count, fill = concentration, label = count)) + 
+  geom_bar(stat = "identity") +
+  xlab("Year") +
+  ylab("Number of Concentrators") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5)); 
+  plot(hard_sciences_plot)
 
 applied_stem <- concentrations %>% 
   filter(concentration %in% c("Statistics", "Computer Science", "Applied Mathematics*"))
@@ -26,6 +30,8 @@ humanities_plot<-
   ggplot(humanities, aes(x = year, y = count, fill = concentration)) + 
   geom_bar(stat = "identity"); plot(humanities_plot)
 
-
-
-
+social_sciences <- concentrations %>% 
+  filter(concentration %in% c("Economics*","Government", "Psychology"))
+social_sciences_plot <- 
+  ggplot(social_sciences, aes(x = year, y = count, fill = concentration)) + 
+  geom_bar(stat = "identity"); plot(social_sciences_plot)
