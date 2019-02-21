d88009fe00e0d102e84ca2cb9945a7ca5e0ce145
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-20 23:41:33 -0500

    Cleaned up variables, add plot for humanities.

--- initial.R
+++ initial.R
@@ -3,19 +3,31 @@ library(readr)
 library(readxl)
 library(ggplot2)
 library(knitr)
+library(janitor)
 
 concentrations <- read_xlsx("concentrations_h.xlsx") %>% 
-  gather(Year, Count, 2:11) 
+  gather(Year, Count, 2:11) %>% 
+  clean_names()
 
 hard_sciences <- concentrations %>% 
-  filter(Concentration %in% c("Molecular and Cellular Biology", "Chemistry", "Physics")) 
+  filter(concentration %in% c("Molecular and Cellular Biology", "Chemistry", "Physics")) 
 hard_sciences_plot<- 
-  ggplot(hard_sciences, aes(x = Year, y = Count, fill = Concentration)) + 
+  ggplot(hard_sciences, aes(x = year, y = count, fill = concentration)) + 
   geom_bar(stat = "identity"); plot(hard_sciences_plot)
 
 applied_stem <- concentrations %>% 
-  filter(Concentration %in% c("Statistics", "Computer Science", "Applied Mathematics*"))
+  filter(concentration %in% c("Statistics", "Computer Science", "Applied Mathematics*"))
 applied_stem_plot<- 
-  ggplot(applied_stem, aes(x = Year, y = Count, fill = Concentration)) + 
+  ggplot(applied_stem, aes(x = year, y = count, fill = concentration)) + 
   geom_bar(stat = "identity"); plot(applied_stem_plot)
+
+
+humanities <- concentrations %>% 
+  filter(concentration %in% c("English", "Comparative Literature"))
+humanities_plot<- 
+  ggplot(humanities, aes(x = year, y = count, fill = concentration)) + 
+  geom_bar(stat = "identity"); plot(humanities_plot)
+
+
+
 
