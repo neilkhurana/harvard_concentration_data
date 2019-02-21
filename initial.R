57d4c2771d955cfa682274c81c125a8d4d605257
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-21 00:00:33 -0500

    Cleaned up axis labels and included numbers within bars for each concentration.

--- initial.R
+++ initial.R
@@ -10,22 +10,40 @@ concentrations <- read_xlsx("concentrations_h.xlsx") %>%
 hard_sciences <- concentrations %>% 
   filter(concentration %in% c("Molecular and Cellular Biology", "Chemistry", "Physics")) 
 hard_sciences_plot<- 
-  ggplot(hard_sciences, aes(x = year, y = count, fill = concentration)) + 
-  geom_bar(stat = "identity"); plot(hard_sciences_plot)
+  ggplot(hard_sciences, aes(x = year, y = count, fill = concentration, label = count)) + 
+  geom_bar(stat = "identity") +
+  xlab("Year") +
+  ylab("Number of Concentrators") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5)); 
+  plot(hard_sciences_plot)
 
 applied_stem <- concentrations %>% 
   filter(concentration %in% c("Statistics", "Computer Science", "Applied Mathematics*"))
 applied_stem_plot<- 
-  ggplot(applied_stem, aes(x = year, y = count, fill = concentration)) + 
-  geom_bar(stat = "identity"); plot(applied_stem_plot)
+  ggplot(applied_stem, aes(x = year, y = count, fill = concentration, label = count)) + 
+  geom_bar(stat = "identity")+
+  xlab("Year") +
+  ylab("Number of Concentrators") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5));
+  plot(applied_stem_plot)
 
 
 humanities <- concentrations %>% 
   filter(concentration %in% c("English", "Comparative Literature"))
 humanities_plot<- 
-  ggplot(humanities, aes(x = year, y = count, fill = concentration)) + 
-  geom_bar(stat = "identity"); plot(humanities_plot)
+  ggplot(humanities, aes(x = year, y = count, fill = concentration, label = count)) + 
+  geom_bar(stat = "identity") +
+  xlab("Year") +
+  ylab("Number of Concentrators") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5));
+  plot(humanities_plot)
 
-
-
-
+social_sciences <- concentrations %>% 
+  filter(concentration %in% c("Economics*","Government", "Psychology"))
+social_sciences_plot <- 
+  ggplot(social_sciences, aes(x = year, y = count, fill = concentration, label = count)) + 
+  geom_bar(stat = "identity") + 
+  xlab("Year") +
+  ylab("Number of Concentrators") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5));
+  plot(social_sciences_plot)
