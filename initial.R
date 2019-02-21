107faba35f85111a6a471e2697a441bf37ebf9eb
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-20 23:24:49 -0500

    Reads in data. Filters for hard sciences and plots to stacked bar graph.

---
+++ initial.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(readr)
+library(readxl)
+library(ggplot2)
+library(knitr)
+
+concentrations <- read_xlsx("concentrations_h.xlsx") %>% 
+  gather(Year, Count, 2:11) 
+
+hard_sciences <- concentrations %>% 
+  filter(Concentration %in% c("Molecular and Cellular Biology", "Chemistry", "Physics")) 
+hard_sciences_plot<- 
+  ggplot(hard_sciences, aes(x = Year, y = Count, fill = Concentration)) + 
+  geom_bar(stat = "identity"); plot(hard_sciences_plot)
+
+
+
